350499c67fa6f535e225ddf844706a79534156a7
sladuf <04fud27@gmail.com>
2022-11-05 22:15:14 +0900

    build: App 초기 세팅

---
+++ Home/Home/Home.h
@@ -0,0 +1,18 @@
+//
+//  Home.h
+//  Home
+//
+//  Created by 김민령 on 2022/11/05.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Home.
+FOUNDATION_EXPORT double HomeVersionNumber;
+
+//! Project version string for Home.
+FOUNDATION_EXPORT const unsigned char HomeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Home/PublicHeader.h>
+
+
---
+++ MinemeApp/MinemeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MinemeApp
+//
+//  Created by 김민령 on 2022/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
