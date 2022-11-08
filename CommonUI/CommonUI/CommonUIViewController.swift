26650504ebd57e8dadb04bc02b7310358a201fd5
sladuf <04fud27@gmail.com>
2022-11-09 02:56:25 +0900

    feat: CommonUIViewController

---
+++ CommonUI/CommonUI/CommonUIViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CommonViewController.swift
+//  CommonUI
+//
+//  Created by 김민령 on 2022/11/08.
+//
+
+import UIKit
+
+protocol CommonUIViewController : UIViewController {
+    func initAttribute()
+    func initAutolayout()
+}
