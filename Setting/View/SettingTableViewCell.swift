1d61c0e0abed4e0a1a42897a575e31f1078e46c8
sladuf <04fud27@gmail.com>
2022-11-29 22:42:15 +0900

    feat: Setting-Main,Account View

--- Setting/View/SettingTableViewCell.swift
+++ Setting/View/SettingTableViewCell.swift
@@ -10,12 +10,16 @@ import SnapKit
 
 class SettingTableViewCell: UITableViewCell {
     
+    static let cellId = "SettingTableViewCell"
+    
+    var vc : UIViewController?
+    
     private var icon = UIImageView()
     private var text = UILabel()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
+        self.backgroundColor = .white
         self.frame.size.height = 50
         initAttribute()
         initAutolayout()
@@ -25,9 +29,10 @@ class SettingTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func initData(text: String, icon: String){
+    func initData(vc: UIViewController? = nil, text: String, icon: String){
+        self.vc = vc
         self.text.text = text
-        self.icon.image = UIImage(named: icon)
+        self.icon.image = UIImage(systemName: icon)
     }
     
     func initAttribute(){
@@ -37,13 +42,15 @@ class SettingTableViewCell: UITableViewCell {
     
     func initAutolayout(){
         
+        [icon, text].forEach { self.addSubview($0) }
+        
         icon.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(21)
             $0.centerY.equalToSuperview()
         }
-        
+
         text.snp.makeConstraints {
-            $0.leading.equalTo(icon)
+            $0.leading.equalTo(icon.snp.trailing).offset(23)
             $0.centerY.equalToSuperview()
         }
     }
--- Setting/View/SettingViewController.swift
+++ Setting/View/SettingViewController.swift
@@ -6,15 +6,79 @@
 //
 
 import UIKit
+import SnapKit
 
 open class SettingViewController: UIViewController {
+    
+    //FIXME: 수정하기
+    let cellData = ["계정", "알림", "보안", "공지사항", "문의하기"]
+    
+    private var titleText = UILabel()
+    private let tableView = UITableView()
 
     open override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+        initAttribute()
+        initAutolayout()
 
         // Do any additional setup after loading the view.
     }
     
+    func initAttribute(){
+        titleText = {
+           let label = UILabel()
+            label.text = "설정"
+            label.textColor = .black
+            label.font = .systemFont(ofSize: 20)
+            label.frame.size = CGSize(width: 199, height: 28)
+            return label
+        }()
+        
+        tableView.backgroundColor = .white
+        
+        tableView.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.cellId)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
     
+    func initAutolayout(){
+        
+        [tableView, titleText].forEach { self.view.addSubview($0) }
+        
+        titleText.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(68)
+            $0.centerX.equalToSuperview()
+        }
+        
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(titleText.snp.bottom).offset(38)
+            $0.bottom.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+    }
 
+}
+
+extension SettingViewController : UITableViewDataSource, UITableViewDelegate {
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellData.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.cellId, for: indexPath) as! SettingTableViewCell
+        cell.initData(vc: AccountVIewController() ,text: cellData[indexPath.row], icon: "person")
+        cell.selectionStyle = .none
+        
+        
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) as? SettingTableViewCell else { return }
+        guard let vc = cell.vc else { return }
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
