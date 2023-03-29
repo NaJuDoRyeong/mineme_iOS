990c36df21f2b65282ed19fa1f8c2d7288d7b9ba
sladuf <04fud27@gmail.com>
2023-03-29 23:27:09 +0900

    refactor: CommonUI -> Common framework로 통합 & 폴더 구조 변경

--- Common/View/Extension/UIImage+Extension.swift
+++ Common/View/Extension/UIImage+Extension.swift
@@ -13,4 +13,13 @@ extension UIImage {
         }
         return nil
     }
+    
+    public func resize(width: CGFloat, height: CGFloat) -> UIImage {
+        let size = CGSize(width: width, height: height)
+        let render = UIGraphicsImageRenderer(size: size)
+        let renderImage = render.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+        return renderImage
+    }
 }
